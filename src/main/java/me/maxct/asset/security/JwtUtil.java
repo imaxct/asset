377b4aad09821620de8fef06e5dc3ac2c88c1407
imaxct <imaxct@hotmail.com>
2019-03-23 20:46:25 +0800

    添加jwtUtil

--- src/main/java/me/maxct/asset/security/JwtUtil.java
+++ src/main/java/me/maxct/asset/security/JwtUtil.java
@@ -2,9 +2,13 @@ package me.maxct.asset.security;
 
 import java.security.Key;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import me.maxct.asset.domain.User;
@@ -15,8 +19,10 @@ import me.maxct.asset.domain.User;
  */
 @Component
 public class JwtUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtUtil.class);
+
     @Value("app.jwtKey")
-    private String jwtKey;
+    private String              jwtKey;
 
     public String sign(User user) {
         Key key = Keys.hmacShaKeyFor(jwtKey.getBytes());
@@ -24,13 +30,18 @@ public class JwtUtil {
             .signWith(key).compact();
     }
 
-    public boolean check(String rawToken) {
+    public User check(String rawToken) {
         Key key = Keys.hmacShaKeyFor(jwtKey.getBytes());
         try {
-            Jwts.parser().setSigningKey(key).parse(rawToken);
-            return true;
+            User user = new User();
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(key).parseClaimsJws(rawToken);
+            Claims claims = claimsJws.getBody();
+            user.setId(Long.valueOf(claims.getId()));
+            user.setUsername(claims.getSubject());
+            return user;
         } catch (Exception e) {
-            return false;
+            LOGGER.error("parse jwt error", e);
         }
+        return null;
     }
 }
